1bf4b30cbb5e6ab8744375a6840841add7ee0b63
Miguel Angel Castañeda Vasquez <mcastaneda@merqueo.com>
2022-12-02 11:06:00 -0500

    feat: modify API response data

--- app/src/main/java/com/example/myapplication/animeapi/response/Images.kt
+++ app/src/main/java/com/example/myapplication/animeapi/response/Images.kt
@@ -4,11 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 
 data class Images (
-  @SerializedName("jpg") var jpg: Jpg?
-//  @SerializedName("image_url"         ) var imageUrl        : String? = null,
-//  @SerializedName("small_image_url"   ) var smallImageUrl   : String? = null,
-//  @SerializedName("medium_image_url"  ) var mediumImageUrl  : String? = null,
-//  @SerializedName("large_image_url"   ) var largeImageUrl   : String? = null,
-//  @SerializedName("maximum_image_url" ) var maximumImageUrl : String? = null
-
+  @SerializedName("jpg") var jpg: Jpg?,
+  @SerializedName("webp") var webp: Webp?
 )
